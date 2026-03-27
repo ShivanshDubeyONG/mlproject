2781d5f2afc66e5a20b34d57e0e7c1a3f03807b1
ShivanshDubeyONG <shivanshdhardubey@gmail.com>
2026-03-27 14:07:19 +0530

    Initial ML Pipeline project done

--- src/components/dataingestion.py
+++ src/components/dataingestion.py
@@ -10,6 +10,8 @@ from dataclasses import dataclass
 from src.components.datatransformation import DataTransformation
 from src.components.datatransformation import DataTransformationConfig
 
+from src.components.modeltrainer import ModelTrainer
+from src.components.modeltrainer import ModelTrainerConfig
 
 
 @dataclass
@@ -51,3 +53,11 @@ class DataIngestion:
 if __name__ == "__main__":
     obj = DataIngestion()
     train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data, test_data)
+
+    modeltrainer = ModelTrainer()
+    r2 = modeltrainer.initiate_model_trainer(train_arr, test_arr)
+
+print(f"Best Model R2 Score: {r2:.4f}")
